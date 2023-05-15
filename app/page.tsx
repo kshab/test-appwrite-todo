1cc2b2375feab596888a95c0fa2eeaf59acd0f4e
Konstantin Shabaliev <kvshabalev@gmail.com>
2023-05-15 21:56:28 +0300

    add appwrite

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,48 @@
-import Image from 'next/image'
+'use client';
+
+import { Client, Databases } from 'appwrite';
 import styles from './page.module.css'
 
 export default function Home() {
+  const fetchTodos = async () => {
+    const client = new Client();
+
+    client
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
+      .setProject(process.env.NEXT_PUBLIC_PROJECT_ID as string);
+
+    const databases = new Databases(client);
+
+    const res = await databases.listDocuments(
+      process.env.NEXT_PUBLIC_DATABASE_ID as string,
+      process.env.NEXT_PUBLIC_COLLECTION_ID as string
+    );
+    const todos = res.documents;
+
+    console.log(todos);
+  };
+
+  const login = (event: any) => {
+    event.preventDefault();
+    const name = event.target[0].value;
+    const password = event.target[1].value;
+  };
+
+  fetchTodos();
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <h2>Test</h2>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <form onSubmit={login}>
+        <label htmlFor='name'></label>
+        <input type="text" name='name' />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <label htmlFor='password'></label>
+        <input type="password" name='password' />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <input type="submit" value='submit' />
+      </form>
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_APPWRITE_ENDPOINT: 'https://cloud.appwrite.io/v1',
+    NEXT_PUBLIC_PROJECT_ID: '64626de26c55b32ddab3',
+    NEXT_PUBLIC_DATABASE_ID: '64627893092375bd5ed9',
+    NEXT_PUBLIC_COLLECTION_ID: '646278a27f36ee872057',
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
